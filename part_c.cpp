169081fb9d9e8a7d11ffc3f59ea30120c8b23f2c
thommann <thomasrolf.mannhart@uzh.ch>
2022-10-03 15:28:43 +0800

    part_c maybe

--- part_c.cpp
+++ part_c.cpp
@@ -1,5 +1,7 @@
 
 #include "Aria.h"
+//#include "Aria/include/Aria.h"
+#include <cstdio>
 
 int main(int argc, char **argv)
 {
@@ -9,7 +11,7 @@ int main(int argc, char **argv)
 	robot.addRangeDevice(&sonar);
 
 	Aria::init();
-	
+
 	ArSimpleConnector connector(&argc,argv);
 
 	if (!connector.connectRobot(&robot)){
@@ -22,15 +24,15 @@ int main(int argc, char **argv)
 
 	robot.runAsync(false);
 
-  // Used to perform actions when keyboard keys are pressed
-  ArKeyHandler keyHandler;
-  Aria::setKeyHandler(&keyHandler);
+	// Used to perform actions when keyboard keys are pressed
+	// ArKeyHandler keyHandler;
+	// Aria::setKeyHandler(&keyHandler);
 
-  // ArRobot contains an exit action for the Escape key. It also 
-  // stores a pointer to the keyhandler so that other parts of the program can
-  // use the same keyhandler.
-  robot.attachKeyHandler(&keyHandler);
-  printf("You may press escape to exit\n");
+	// ArRobot contains an exit action for the Escape key. It also
+	// stores a pointer to the keyhandler so that other parts of the program can
+	// use the same keyhandler.
+	// robot.attachKeyHandler(&keyHandler);
+	// printf("You may press escape to exit\n");
 
 	// TODO: control the robot
 
@@ -39,17 +41,29 @@ int main(int argc, char **argv)
 	// 1. Lock the robot
 	robot.lock();
 
-	// 2. Write your control code here, 
-	//    e.g. robot.setVel(150);  
-	robot.setVel(150);
+	// 2. Write your control code here,
+	//    e.g. robot.setVel(150);
+	robot.setVel(0);
 
 	// 3. Unlock the robot
 	robot.unlock();
 
 	// 4. Sleep a while and let the robot move
+	int c;
 	while(true){
+		c = getchar()
+		switch(c){
+			case 'w':
+				robot.setVel(robot.getVel() + 10);
+				break;
+			case 's':
+				robot.setVel(robot.getVel() - 10);
+				break;
+			case ' ':
+				robot.stop()
+		}
 		printf("%f %f %f\n", robot.getX(), robot.getY(), robot.getTh());
-		ArUtil::sleep(300);
+		ArUtil::sleep(1000);
 	}
 
 	// End of controling
