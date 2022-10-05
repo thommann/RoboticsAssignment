66fa3fa3b90747f93f23611b1a32196e5360490f
thommann <thomasrolf.mannhart@uzh.ch>
2022-10-05 15:29:27 +0800

    part e

--- part_e.cpp
+++ part_e.cpp
@@ -57,20 +57,21 @@ int main(int argc, char **argv)
 	double initial_y = 3580;
 	double initial_th = 3093.97;
 
-	double delta_x = target_x - initial_x;
-	double delta_y = target_y - initial_y;
+	double delta_x = initial_x - target_x;
+	double delta_y = initial_y - target_y;
 
 	double distance = sqrt(pow(delta_x, 2) + pow(delta_y, 2));
 
 	double angle = atan2(delta_y, delta_x);
-	printf("Angle: %f", angle);
-	printf("Distance: %f", distance);
+
+	printf("Angle: %f\n", angle);
+	printf("Distance: %f\n", distance);
 
 	ArPose currentPose(initial_x, initial_y, initial_th);
 	robot.moveTo(currentPose);
 
 	robot.setHeading(angle);
-	ArUtil::sleep(5000);
+	ArUtil::sleep(6000);
 
 	robot.move(distance);
 
