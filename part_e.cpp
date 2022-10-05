b8bd327cb280214381fd81f1f34e1ea49ea61259
thommann <thomasrolf.mannhart@uzh.ch>
2022-10-05 14:45:33 +0800

    part e

---
+++ part_e.cpp
@@ -0,0 +1,70 @@
+
+#include "Aria.h"
+//#include "Aria/include/Aria.h"
+#include <cstdio>
+
+int main(int argc, char **argv)
+{
+	ArRobot robot;
+	ArSonarDevice sonar;
+
+	robot.addRangeDevice(&sonar);
+
+	Aria::init();
+
+	ArSimpleConnector connector(&argc,argv);
+
+	if (!connector.connectRobot(&robot)){
+		printf("Could not connect to robot... exiting\n");
+		Aria::shutdown();
+		Aria::exit(1);
+	}
+
+	robot.comInt(ArCommands::ENABLE, 1);
+
+	robot.runAsync(false);
+
+	// Used to perform actions when keyboard keys are pressed
+	// ArKeyHandler keyHandler;
+	// Aria::setKeyHandler(&keyHandler);
+
+	// ArRobot contains an exit action for the Escape key. It also
+	// stores a pointer to the keyhandler so that other parts of the program can
+	// use the same keyhandler.
+	// robot.attachKeyHandler(&keyHandler);
+	// printf("You may press escape to exit\n");
+
+	// TODO: control the robot
+
+	// Start of controling
+
+	// 1. Lock the robot
+	robot.lock();
+
+	// 2. Write your control code here,
+	//    e.g. robot.setVel(150);
+	robot.setVel(0);
+
+	// 3. Unlock the robot
+	robot.unlock();
+
+	// 4. Read the command sequence and execute it.
+	double target_x = 7000;
+	double target_y = 5000;
+	double target_th = 90;
+
+	while(true){
+		double odo_x = robot.getX();
+		double odo_y = robot.getY();
+		double odo_th = robot.getTh();
+
+		printf("Odometry: %f %f %f\n", odo_x, odo_y, odo_th);
+		ArUtil::sleep(300);
+	}
+
+	// End of controling
+
+	Aria::shutdown();
+
+	Aria::exit(0);
+}
