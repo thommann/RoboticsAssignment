de8273d10d028fbce192a8788ba1abc8110aecc3
thommann <thomasrolf.mannhart@uzh.ch>
2022-10-06 10:43:29 +0800

    bonus

--- bonus.cpp
+++ bonus.cpp
@@ -148,7 +148,7 @@ int main(int argc, char **argv)
 			double reading_right = sonar.cumulativeReadingPolar(0, 20);
 			double reading_max = max(reading_left, reading_right);
 			if(reading_max < 2000 && reading_max < 5 * robot.getVel()){
-				printf("Obstacle: %f\n", reading);
+				printf("Obstacle: %f %f\n", reading_left, reading_right);
 				robot.stop();
 				waitForMove(robot);
 				obstacle = 1;
