2f612d661298f135733c1a82014aabbb10cdddb2
thommann <thomasrolf.mannhart@uzh.ch>
2022-10-06 10:22:04 +0800

    bonus

--- bonus.cpp
+++ bonus.cpp
@@ -145,14 +145,14 @@ int main(int argc, char **argv)
 		while (true) {
 			ArUtil::sleep(500);
 			double reading = sonar.cumulativeReadingPolar(-20, 20);
-			if(reading < 1000){
+			if(reading < 200){
 				printf("Obstacle: %f\n", reading);
 				robot.stop();
 				waitForMove(robot);
 				obstacle = 1;
 				robot.setDeltaHeading(90);
 				waitForRot(robot);
-				robot.move(1000);
+				robot.move(200);
 				waitForMove(robot);
 				break;
 			}
