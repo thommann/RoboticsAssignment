d7c96ae712f78a76565d9710910da45b74179c73
thommann <thomasrolf.mannhart@uzh.ch>
2022-10-23 21:38:59 +0800

    v2

--- Assignment2/partc_v2.m
+++ Assignment2/partc_v2.m
@@ -31,7 +31,6 @@ T_4_5 = [ c5, -s5,   0,     0;
            0,   0,   0,     1];
 
 T_0_5 = T_0_1 * T_1_2 * T_2_3 * T_3_4 * T_4_5;
-T_0_5
 
 % Part C
 
@@ -66,14 +65,50 @@ R = subs(R, ...
 
 % Pose
 H = [[R; 0, 0, 0] [t; 1]];
-H
+
+% syms r11 r12 r13 r21 r22 r23 r31 r32 r33 x y z
+% H = [r11, r12, r13, x;
+%      r21, r22, r23, y;
+%      r31, r32, r33, z;
+%        0,   0,   0, 1];
 
 % Equations
-EQ = T_0_5 == H;
+EQ_0_5 = T_0_5 == H;
+
+T_1_5 = T_1_2 * T_2_3 * T_3_4 * T_4_5;
+H_1_5 = T_0_1\H;
+
+EQ = T_1_5 == H_1_5;
+EQ = subs(EQ, c1^2 + s1^2, 1);
 EQ
 
-% solution = solve(EQ, [s1, s2, s3, s4, s5, c1, c2, c3, c4, c5]);
+
+% Equate elements (2,4) from both sides:
+eq1 = EQ(2, 4);
+rho = sqrt(x^2 + y^2);
+theta1 = atan2(y, x) - atan2(d4, rho * sqrt(1-(d4/rho)^2));
+theta1
+
+
+% Equate elements (2,1) and (2, 2) from both sides:
+eq5 = EQ(2, 1:2);
+solution5 = solve(eq5, [s5, c5]);
+theta5 = atan2(solution5.s5, solution5.c5);
+theta5
+
+
+
+
+
+
+
+
+
+
+% solution = solve(EQ_1_5_sub, [s1, s2, s3, s4, s5, c1, c2, c3, c4, c5]);
 % solution
+
+
 
 
 
