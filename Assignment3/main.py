f50abaadc555c8dfcdaf031b4eedc6e68f403474
thommann <thomasrolf.mannhart@uzh.ch>
2022-11-03 13:43:18 +0800

    image set 2

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -4,51 +4,96 @@ import cv2 as cv
 import numpy as np
 
 
-def undistort(_fname, _mtx, _dist):
-    _img = cv.imread(_fname)
+def undistort(_file_name, _mtx, _dist, save_uncropped=False):
+    """
+    Undistort the image with the given filename using the given camera parameters.
+    :param _file_name: filename of the image
+    :param _mtx: camera matrix
+    :param _dist: distortion coefficients
+    :param save_uncropped: save uncropped image
+    :return: None
+    """
+    _img = cv.imread(_file_name)
     h, w = _img.shape[:2]
     new_camera_mtx, roi = cv.getOptimalNewCameraMatrix(_mtx, _dist, (w, h), 1, (w, h))
-    # undistort
     dst = cv.undistort(_img, _mtx, _dist, None, new_camera_mtx)
-    # crop the image
+
+    if save_uncropped:
+        cv.imwrite(f'undistorted/{_file_name[:-4]}-uncropped.png', dst)
+
+    # Crop image
     x, y, w, h = roi
     dst = dst[y:y + h, x:x + w]
-    cv.imwrite(f'undistorted/{_fname[:-4]}-undistorted.png', dst)
+    cv.imwrite(f'undistorted/{_file_name[:-4]}-undistorted.png', dst)
 
 
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+def find_image_points(_file_name, draw_image_points=False):
+    """
+    Fin the image points on the picture of a checkerboard.
+    :param _file_name: filename of the image
+    :param draw_image_points: save the image with the corners drawn onto it
+    :return: tuple with boolean if process was successful and image points if it was
+    """
+    _imgp = None
+    image = cv.imread(_file_name)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    success, corners = cv.findChessboardCorners(gray, (8, 6), None)
+    if success:
+        _imgp = cv.cornerSubPix(gray,
+                                corners,
+                                (11, 11),
+                                (-1, -1),
+                                (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001))
+        if draw_image_points:
+            cv.drawChessboardCorners(image, (8, 6), _imgp, True)
+            cv.imwrite(f'{_file_name[:-4]}-corners.png', image)
+    return success, _imgp
+
+
+# Image Set
+SET = 2
+
+# Checkerboard grid
 objp = np.zeros((6 * 8, 3), np.float32)
 objp[:, :2] = np.mgrid[0:8, 0:6].T.reshape(-1, 2)
-# Arrays to store object points and image points from all the images.
-objpoints = []  # 3d point in real world space
-imgpoints = []  # 2d points in image plane.
-images = glob.glob('images/*.png')
-grays = []
-for fname in images:
-    img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    grays.append(gray)
 
-for gray in grays:
-    # Find the chess board corners
-    ret, corners = cv.findChessboardCornersSB(gray, (8, 6), None)
-    # If found, add object points, image points (after refining them)
+# Array of checkerboard grids
+object_points = []
+# Array of checkerboard corners found in distorted images
+image_points = []
+# Find all PNG files in folder
+file_names = glob.glob(f'images{SET}/*.png')
+# Save an image with found corners for the first image
+draw_corners = True
+# Find corners for all distorted images
+for file_name in file_names:
+    # Find corners
+    ret, imgp = find_image_points(file_name, draw_image_points=draw_corners)
     if ret:
-        print("SUCCESS")
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-        imgpoints.append(corners2)
+        # If corners are found, append to arrays
+        object_points.append(objp)
+        image_points.append(imgp)
+        # Draw corners only for first successful image
+        draw_corners = False
 
-shape = grays[0].shape[::-1]
+# Get camera parameters
+shape = cv.imread(file_names[0]).shape[-2:-4:-1]
+_, camera_matrix, distortion_coefficients, rotation_vectors, translation_vectors = cv.calibrateCamera(object_points,
+                                                                                                      image_points,
+                                                                                                      shape,
+                                                                                                      None,
+                                                                                                      None)
 
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, shape, None, None)
+# Do not crop first image
+save_uncropped = True
+# Undistort checkerboard images
+for file_name in file_names:
+    undistort(file_name, camera_matrix, distortion_coefficients, save_uncropped=save_uncropped)
+    save_uncropped = False
 
-for fname in images:
-    undistort(fname, mtx, dist)
+# Undistort extra image
+extra_img = glob.glob(f'extra{SET}.png')[0]
+undistort(extra_img, camera_matrix, distortion_coefficients, save_uncropped=True)
 
-classroom = glob.glob('classroom.png')[0]
-undistort(classroom, mtx, dist)
-
-print("DONE")
+# Print camera matrix and
+print(f"DONE -> MTX: {camera_matrix}, DIST: {distortion_coefficients}")
