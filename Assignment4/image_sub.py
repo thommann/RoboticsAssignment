52cc1a20ce38045ba7a28c24d21cc290dd8ff839
Edward Lee <34258587+EdwardLeeMacau@users.noreply.github.com>
2022-11-25 00:43:52 +0800

    Update image_sub.py

--- Assignment4/image_sub.py
+++ Assignment4/image_sub.py
@@ -17,33 +17,17 @@ from tm_msgs.srv import *
 
 from sensor_msgs.msg import Image
 from . import shapes
+from .shapes import rotate2D, img2Camera, T
 import cv2
 
-# from scipy.spatial.transfrom import Rotation as R
-
-
-camera_matrix = np.array([[1.37792826e+03, 0.00000000e+00, 6.59752738e+02],
-                          [0.00000000e+00, 1.37632357e+03, 5.44888633e+02],
-                          [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
-x_arm = 350
-y_arm = 350
-z_arm = 730
-phi = 135
-camera_coord = np.array([0, -85, 0])
-
-def rotate2D(phi: float) -> np.ndarray:
-    phi = m.radians(phi)
-    return np.array([[m.cos(phi), -m.sin(phi), 0],
-                     [m.sin(phi), m.cos(phi), 0],
-                     [0, 0, 1]])
 
 class ImageSub(Node):
     def __init__(self, nodeName):
         super().__init__(nodeName)
-        self.subscription = self.create_subscription(Image, 
+        self.subscription = self.create_subscription(Image,
         'techman_image', self.image_callback, 10)
         self.subscription
-    
+
     def image_callback(self, data):
         self.get_logger().info('Received image')
 
@@ -53,25 +37,17 @@ class ImageSub(Node):
         objs: List = shapes.detect(img)
         self.get_logger().info(str(objs))
 
-
+        # TODO: Tune robot arm orientation (rz)
         for object in objs:
-            x, y, a = object
-            image_coord = np.array([x, y, 1])
-            # cam_rot = R.from_euler('xyz', [-180, 0, 135], degrees=True)
-            # cam_trans = np.array([x_arm, y_arm, z_arm])
-            # f_camera_matrix = camera_matrix * np.concatenate(cam_rot, cam_trans.T, axis=1)
-            world_coord = (np.linalg.inv(camera_matrix) @ image_coord) * z_arm
-            cam_trans = rotate2D(-phi) @ camera_coord
-            rotated_point = rotate2D(-phi) @ world_coord
-            final_coord = rotated_point + cam_trans + np.array([x_arm, y_arm, 0])
-            targetP1 = f"{final_coord[0]}, {final_coord[1]}, 200, -180.00, 0.0, 135.00"
-            # script1 = "PTP(\"CPP\","+targetP1+",100,200,0,false)"
-            # send_script(script1)
-            self.get_logger().info(f'INV: {np.linalg.inv(camera_matrix)}')
-            self.get_logger().info(f'IMAGE: {image_coord}')
-            self.get_logger().info(f'WORLD: {world_coord}')
-            self.get_logger().info(f'FINAL: {final_coord}')
-            
+            x, y, z = img2Camera(object)
+            x, y, z, _ = T @ np.array([x, y + 85, z, 1])
+
+            self.get_logger().info(f"Target: {x}, {y}, {z}")
+
+            frame = f"{x:.0f}, {y:.0f}, 200, -180.00, 0.0, 135.00"
+            script_ptp = "PTP(\"CPP\"," + frame + ",100,200,0,false)"
+
+            send_script(script_ptp)
 
 
 def send_script(script):
@@ -92,7 +68,7 @@ def set_io(state):
 
     while not gripper_cli.wait_for_service(timeout_sec=1.0):
         node.get_logger().info('service not availabe, waiting again...')
-    
+
     io_cmd = SetIO.Request()
     io_cmd.module = 1
     io_cmd.type = 1
